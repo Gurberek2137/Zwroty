0be1cabd5c248f2c7d398bc1a3e383fce1a855aa
Gurberek2137 <dawid.cudak09@gmail.com>
2026-09-10 12:37:18 +0200

    Przekierowanie zakupu produktow na sklep eBay Letino Outlet (script.js)

--- script.js
+++ script.js
@@ -3,8 +3,14 @@
  */
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Pełny link do Twojego profilu Instagram
+  // Oficjalne linki Letino Outlet
   const INSTAGRAM_URL = 'https://www.instagram.com/letino.outlet?igsh=dW1zdHdwbDJkbzJr&utm_source=qr';
+  const EBAY_URL = 'https://www.ebay.de/str/letinooutlet?mkcid=16&mkevt=1&mkrid=707-127634-2357-0&ssspo=bovaat-vsa-&sssrc=3418065&ssuid=bovaat-vsa-&stype=1&widget_ver=artemis&media=COPY';
+
+  // Aktualizacja linków zakupu produktów na eBay
+  document.querySelectorAll('.card-link').forEach(link => {
+    link.href = EBAY_URL;
+  });
 
   // Aktualizacja linków Instagram na stronie
   document.querySelectorAll('a[href*="instagram.com"]').forEach(link => {
